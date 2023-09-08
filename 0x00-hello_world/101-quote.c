8f9e6157956b4145bd8b10001282c31fc3d0613c
Zeecaptcha <ayomiposijimoh@gmail.com>
2023-09-08 02:48:33 +0100

    betty keeps pissng me off

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+/**
+ * Definition: main - Starts the program
+ * Return: 1
+ */
+
+int main(void)
+{
+write(1, "and that piece of art is useful\""" - Dora Korpar, 2015-10-19"
+, 59);
+write(1, "\n", 1);
+return (1);
+}
---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+
+/**
+ * Description: main - Its prints out a string to standard output
+ * Return: 0
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
