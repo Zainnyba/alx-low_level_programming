52a44e18138462f467c7f24750d9bb5526655b95
Zeecaptcha <ayomiposijimoh@gmail.com>
2023-09-12 05:53:24 +0100

    last question

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,25 +1,31 @@
 #include <stdio.h>
 
+
 /**
  * main - prints all possible combinations of two two-digit numbers
  * Return: 0
-*/
+ */
 int main(void)
 {
-int a, b, c, d, e = 0, f = 0;
-for (a = 48; a < 58; a++, e++)
+int a, b, c, d;
+
+for (a = 0; a < 100; a++)
 {
-b = 48 + e;
-for ( ; b < 58; b++)
+for (b = 0; b < 100; b++)
 {
-for (c = 48; c < 58; c++, f++)
+if (a < b)
 {
-d = 48 + f;
-for ( ; d < 58; d++)
+putchar((a / 10) + '0');
+putchar((a % 10) + '0');
+putchar(' ');
+putchar((b / 10) + '0');
+putchar((b % 10) + '0');
+
+if (a != 98 || b != 99)
 {
-putchar(a), putchar(b), putchar(32), putchar(c), putchar(d);
-if (a != 55 || b != 56 || c != 57 || d != 57)
-putchar(44), putchar(32); }}}}
+putchar(',');
+putchar(' ');
+}}}}
 putchar('\n');
 return (0);
 }
