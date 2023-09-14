5b8ef55c220b700641e2808362e9cf34ad2a8fd1
Zeecaptcha <ayomiposijimoh@gmail.com>
2023-09-14 17:15:01 +0100

    really thought I was done

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -4,20 +4,24 @@
  * main - nothing much, just fizzing and buzzing
  * Return: 0
  */
+
 int main(void)
 {
 int i = 1;
 
-for ( ; i < 101; i++)
+for (; i < 101; i++)
 {
 if ((i % 3 == 0) && (i % 5 != 0))
-printf("Fizz ");
+printf("Fizz");
 else if ((i % 5 == 0) && (i % 3 != 0))
-printf("Buzz ");
+printf("Buzz");
 else if ((i % 3 == 0) && (i % 5 == 0))
-printf("FizzBuzz ");
+printf("FizzBuzz");
 else
-printf("%d ", i);
+printf("%d", i);
+
+if (i < 100)
+printf(" ");
 }
 putchar('\n');
 return (0);
