72c998efd35baaf8a904b98627566b6f21a5b5e6
Zeecaptcha <ayomiposijimoh@gmail.com>
2023-09-14 16:11:37 +0100

    debugginggggggggggg

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,12 +1,16 @@
 #include "main.h"
 
 /**
-* main - positive_or_negative function tester
+* main - tests function that prints if integer is positive or negative
 * Return: 0
 */
+
 int main(void)
 {
-int i = 0;
+int i;
+
+i = 0;
 positive_or_negative(i);
+
 return (0);
 }
