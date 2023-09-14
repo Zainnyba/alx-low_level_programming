0bb723a7bb712f34d3ada3490bdafc358df4d08c
Zeecaptcha <ayomiposijimoh@gmail.com>
2023-09-14 05:57:00 +0100

    okayyy

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,22 +1,22 @@
-
 #include "main.h"
 
 /**
- * main - prints the first fifty fibonacci numbers
- * Return: 0
+* main - prints out first 50 fibonacci suit numbers
+* Return: 0
 */
 int main(void)
 {
-size_t a = 1, b = 2, fib, i = 0;
-printf("1, 2, ");
-for ( ; i < 48; i++)
+int i;
+unsigned long num1 = 0, num2 = 1, num3;
+for (i = 0; i < 50; i++)
 {
-fib = a + b;
-a = b, b = fib;
-printf("%ld", fib);
-if (i != 47)
-printf(", ");
-}
-putchar('\n');
-return (0);
-}
+num3 = num1 + num2;
+printf("%lu", num3);
+num1 = num2;
+num2 = num3;
+
+if (i == 49)
+printf("\n");
+else
+printf(", "); }
+return (0); }
