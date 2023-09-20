e97e663599508fd43a7b68d702ea8e5b26759f6a
Zeecaptcha <111015250+Zeecaptcha@users.noreply.github.com>
2023-09-20 03:25:17 +0100

    fixed some misakes

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,14 +1,18 @@
 #include "main.h"
 
 /**
- * puts2 - prints every other character of of a string
- * @str: a pointer to a string
-*/
+ * puts2 - prints every other character of a string
+ * @str: pointer to string to be printed
+ **/
 void puts2(char *str)
 {
 int i = 0;
-while (str[i])
-_putchar(str[i]), i += 2;
-_putchar(10);
+while (str[i] != '\0')
+{
+if (i % 2 == 0)
+_putchar(str[i]);
+i++;
 }
 
+_putchar('\n');
+}
